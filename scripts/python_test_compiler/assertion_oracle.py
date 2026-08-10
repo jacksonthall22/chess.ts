2af9d41b77f911ab6f8161571bfcfe4106adcf80
Jackson Hall <jacksonthall22@gmail.com>
2026-08-09 20:58:42 -0400

    sync: mirror python-chess d625be1d
    python-chess-commit: d625be1da24ddd18e27a44c42c6bbb53ae308edc

--- scripts/python_test_compiler/assertion_oracle.py
+++ scripts/python_test_compiler/assertion_oracle.py
@@ -42,7 +42,7 @@ GENERATED_ASSERTION_ORACLE = (
     / "test"
     / "python-assertion-oracle.generated.ts"
 )
-UPSTREAM_COMMIT = "100b1c8b21846c0a9db2f237621368e02294da5f"
+UPSTREAM_COMMIT = "d625be1da24ddd18e27a44c42c6bbb53ae308edc"
 _FROZEN_TEST_MODULE = "_chess_ts_frozen_test_assertion_oracle"
 
 
