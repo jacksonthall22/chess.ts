88c68827de265c95eba72e3f01b0213cb33b33e3
Jackson Hall <jacksonthall22@gmail.com>
2026-08-09 20:33:56 -0400

    sync: mirror python-chess ec399d1b
    Upstream-Commit: ec399d1b0298353b1eee7bf63df03401a00a409a

--- chess/index.ts
+++ chess/index.ts
@@ -2142,7 +2142,7 @@ export class BaseBoard {
   }
 }
 
-export class _BoardState<BoardT extends Board> {
+export class _BoardState {
   pawns: Bitboard
   knights: Bitboard
   bishops: Bitboard
@@ -2159,7 +2159,7 @@ export class _BoardState<BoardT extends Board> {
   halfmoveClock: number
   fullmoveNumber: number
 
-  constructor(board: BoardT) {
+  constructor(board: Board) {
     this.pawns = board.pawns
     this.knights = board.knights
     this.bishops = board.bishops
@@ -2180,7 +2180,7 @@ export class _BoardState<BoardT extends Board> {
     this.fullmoveNumber = board.fullmoveNumber
   }
 
-  restore(board: BoardT) {
+  restore(board: Board) {
     board.pawns = this.pawns
     board.knights = this.knights
     board.bishops = this.bishops
@@ -2318,7 +2318,7 @@ export class Board extends BaseBoard {
    */
   moveStack: Move[]
 
-  _stack: _BoardState<this>[]
+  _stack: _BoardState[]
 
   constructor(
     fen: string | null = STARTING_FEN,
@@ -3104,10 +3104,6 @@ export class Board extends BaseBoard {
     return false
   }
 
-  _boardState(): _BoardState<this> {
-    return new _BoardState(this)
-  }
-
   _pushCapture(
     move: Move,
     captureSquare: Square,
@@ -3142,7 +3138,7 @@ export class Board extends BaseBoard {
   push(move: Move): void {
     // Push move and remember board state.
     move = this._toChess960(move)
-    const boardState = this._boardState()
+    const boardState = new _BoardState(this)
     this.castlingRights = this.cleanCastlingRights() // Before pushing stack
     this.moveStack.push(
       this._fromChess960(
@@ -3280,7 +3276,7 @@ export class Board extends BaseBoard {
       throw new Error('IndexError')
     }
     const move = this.moveStack.pop() as Move
-    ;(this._stack.pop() as _BoardState<this>).restore(this)
+    ;(this._stack.pop() as _BoardState).restore(this)
     return move
   }
 
